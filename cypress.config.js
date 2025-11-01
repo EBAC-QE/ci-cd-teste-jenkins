a8a9ea1dd6ce642fa9af0c90dd48871f01dc8104
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2025-11-01 16:00:24 -0300

    evidencias8

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    video: true
-  },
+
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'mochawesome-report',
     overwrite: false,
     html: true,
     json: true
-  }
-
+  }, 
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    video: true
+  },
 });
